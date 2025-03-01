da301c76c6e9a7d75c84d13583bdc87c0255dec9
HKRK8569 <kayann3021@gmail.com>
2025-03-02 01:45:37 +0900

    monorepoの作成

---
+++ backend/src/index.ts
@@ -0,0 +1,38 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { hc } from 'hono/client'
+import { cors } from 'hono/cors'
+
+const app = new Hono()
+
+app.use("*",
+  cors({
+    "origin": "*"
+  })
+)
+
+const appRouter = app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+.get("/json",(c)=>{
+  return c.json({
+    test: "test"
+  })
+})
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
+
+export type AppRouteType = typeof appRouter;
+
+type ClientType = typeof hc<AppRouteType>;
+
+export const createClient = (
+  ...args: Parameters<ClientType>
+): ReturnType<ClientType> => {
+  return hc<AppRouteType>(...args);
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import "./App.css";
+import { AppRouteType } from "@hono-vite-rpc/backend";
+import { hc } from "hono/client";
+
+function App() {
+  useEffect(() => {
+    const fetchApi = async () => {
+      const client = hc<AppRouteType>("http://localhost:3000/");
+      const response = await client.json.$get();
+      console.log(response);
+    };
+    fetchApi();
+  }, []);
+
+  return <></>;
+}
+
+export default App;
