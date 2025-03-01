d783b1c2dfa98742e53fd990c8fdbd182a20fb3d
HKRK8569 <kayann3021@gmail.com>
2025-03-02 02:04:53 +0900

    不要な個所の削除

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -28,11 +28,3 @@ serve({
 })
 
 export type AppRouteType = typeof appRouter;
-
-type ClientType = typeof hc<AppRouteType>;
-
-export const createClient = (
-  ...args: Parameters<ClientType>
-): ReturnType<ClientType> => {
-  return hc<AppRouteType>(...args);
-}
