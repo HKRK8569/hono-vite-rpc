153b0d029b370217502745cbb21c9dfcf1dd6762
HKRK8569 <kayann3021@gmail.com>
2025-03-07 00:31:52 +0900

    biomeのインストール

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,34 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
 
-const app = new Hono()
+const app = new Hono();
 
-app.use("*",
-  cors({
-    "origin": "*"
-  })
-)
+app.use(
+	"*",
+	cors({
+		origin: "*",
+	}),
+);
 
-const appRouter = app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-.get("/json",(c)=>{
-  return c.json({
-    test: "test"
-  })
-})
+const appRouter = app
+	.get("/", (c) => {
+		return c.text("Hello Hono!");
+	})
+	.get("/json", (c) => {
+		return c.json({
+			test: "test",
+		});
+	});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+serve(
+	{
+		fetch: app.fetch,
+		port: 3000,
+	},
+	(info) => {
+		console.log(`Server is running on http://localhost:${info.port}`);
+	},
+);
 
 export type AppRouteType = typeof appRouter;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,19 @@
 import { useEffect } from "react";
 import "./App.css";
-import { AppRouteType } from "@hono-vite-rpc/backend";
+import type { AppRouteType } from "@hono-vite-rpc/backend";
 import { hc } from "hono/client";
 
 function App() {
-  useEffect(() => {
-    const fetchApi = async () => {
-      const client = hc<AppRouteType>("http://localhost:3000/");
-      const response = await client.json.$get();
-      console.log(response);
-    };
-    fetchApi();
-  }, []);
+	useEffect(() => {
+		const fetchApi = async () => {
+			const client = hc<AppRouteType>("http://localhost:3000/");
+			const response = await client.json.$get();
+			console.log(response);
+		};
+		fetchApi();
+	}, []);
 
-  return <></>;
+	return <></>;
 }
 
 export default App;
